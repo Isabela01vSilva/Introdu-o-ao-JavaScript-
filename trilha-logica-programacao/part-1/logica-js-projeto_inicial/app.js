7ec3e02fd257504b921e00ac7a6894906dd169e2
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-06-17 11:18:53 -0300

    while e contagem de tentativas

--- trilha-logica-programacao/part-1/logica-js-projeto_inicial/app.js
+++ trilha-logica-programacao/part-1/logica-js-projeto_inicial/app.js
@@ -1,10 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-    console.log(`Acertou, ${numeroSecreto}`);
-}else{
-    alert('Errou')
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto 
+    if (chute == numeroSecreto) {
+        alert(`Acertou, ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`o número secreto é menor que ${chute}`);
+        } else {
+            alert(`o número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1
+        tentativas++;
+    }
 }
